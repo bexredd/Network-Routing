54c583f5990d07ae7800e4bcdd120efcecfad39f
rredd <rredd@DESKTOP-PFI378Q>
2019-02-28 12:01:35 -0700

    adding project to git, so far, only added the initial steps of Dijkstras

---
+++ proj3-network-routing/NetworkRoutingSolver.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+
+from CS312Graph import *
+import time
+import math
+
+
+class NetworkRoutingSolver:
+    def __init__( self ):
+        pass
+
+    def initializeNetwork( self, network ):
+        assert( type(network) == CS312Graph )
+        self.network = network
+
+    def getShortestPath( self, destIndex ):
+        self.dest = destIndex
+
+        # TODO: RETURN THE SHORTEST PATH FOR destIndex
+        #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
+        #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL 
+        #       NEED TO USE
+
+        path_edges = []
+        total_length = 0
+        node = self.network.nodes[self.source]
+        edges_left = 3
+        while edges_left > 0:
+            edge = node.neighbors[2]
+            path_edges.append( (edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)) )
+            total_length += edge.length
+            node = edge.dest
+            edges_left -= 1
+        return {'cost':total_length, 'path':path_edges}
+
+    def computeShortestPaths( self, srcIndex, use_heap=False ):
+        self.source = srcIndex
+        t1 = time.time()
+
+        # create a list to hold the distances and set all to infinity. Also set source to 0
+        # also create a list to hold the previous node for each update.
+        distances= [math.inf] * len(self.network.nodes)
+        distances[srcIndex] = 0
+        previous= [] * len(self.network.nodes)
+
+        # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
+        #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
+        #       CALL TO getShortestPath(dest_index)
+
+
+        t2 = time.time()
+        return (t2-t1)
+
