c259b24ace861cd9ccc3fa30c9660571db59a13e
rredd <rredd@DESKTOP-PFI378Q>
2019-03-03 22:28:51 -0700

    refactoring inorder to introduce heap implementation

--- proj3-network-routing/NetworkRoutingSolver.py
+++ proj3-network-routing/NetworkRoutingSolver.py
@@ -20,33 +20,45 @@ class NetworkRoutingSolver:
     def getShortestPath( self, destIndex ):
         self.dest = destIndex
 
-        # ****************Access source index through self.source
-        # TODO: RETURN THE SHORTEST PATH FOR destIndex
-        #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
-        #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL 
-        #       NEED TO USE
-
+        # set up edges and length to return
         path_edges = []
         total_length = 0
 
+        # start at the destination node and move through previous array.
         index = destIndex
         prev = self.previous_nodes[destIndex]
+
         while prev != math.inf:
             total_length = total_length + self.distances[index];
+
+            # get edge, add to path and add length to total length
             edge_index = next((i for i, n in enumerate(self.network.nodes[prev].neighbors) if n.dest.node_id == index), -1)
             edge = self.network.nodes[prev].neighbors[edge_index]
             path_edges.append( (edge.src.loc, edge.dest.loc, '{:.0f}'.format(edge.length)) )
+
+            # move to prev node and repeat.
             index = prev
             prev = self.previous_nodes[index]
-        if index != self.source:
-            return {'cost': math.inf, 'path':path_edges}
-        return {'cost':total_length, 'path':path_edges}
 
-    def computeShortestPaths( self, srcIndex, use_heap=False ):
+        # if you didn't get back to the source the node is unreachable
+        if index != self.source:
+            return {'cost': math.inf, 'path': path_edges}
+        return {'cost': total_length, 'path': path_edges}
+
+    def computeShortestPaths( self, srcIndex, use_heap):
         self.source = srcIndex
         t1 = time.time()
 
-        # create a list to hold the distances and set all to infinity. Also set source to 0
+        if use_heap:
+            pass
+        else:
+            self.array_shortest_paths(srcIndex)
+
+        t2 = time.time()
+        return t2-t1
+
+    def array_shortest_paths(self, srcIndex):
+                # create a list to hold the distances and set all to infinity. Also set source to 0
         # also create a list to hold the previous node index for each update.
         distances= [math.inf] * len(self.network.nodes)
         distances[srcIndex] = 0
@@ -77,8 +89,6 @@ class NetworkRoutingSolver:
         # set distance and previous nodes for use in get shortest path
         self.distances = distances
         self.previous_nodes = previous
-        t2 = time.time()
-        return t2-t1
 
     def array_delete_min(self, nodes):
             # return node of smallest distance and updated node list. Then delete the node from the queue
